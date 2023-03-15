6769f7b8831ff5376a6296f7c5cca78aa7312db0
Lasse Lund Sten Jensen <llzj@novonordisk.com>
2023-03-15 13:11:26 +0100

    feat: Added script for generating a proper unique verification report name based on contextual arguments."

---
+++ templates/scripts/report/get_verification_report_name_for_context.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# This script attempt to generate a verification report name based on the given arguments.
+#
+# arg1: Environment name
+# arg2: Build ID
+# arg3: Ready for [use|production]
+#
+# Example usage:
+# ./get_verification_report_name_for_context.sh "ramone-service1-val-eu-central1" "617829" "use"
+# ./get_verification_report_name_for_context.sh "ramone-service1-val-eu-central1" "617829" "production"
+
+
+#################
+### FUNCTIONS ###
+#################
+
+# Takes parameters: "$ENVIRONMENT_NAME" "$BUILD_ID" "$READY_FOR"
+generate_verification_report_name () {
+    # Determine execution context
+    if [ "$READY_FOR" == "production" ]; then
+        # Context: Generated for production environment
+        # Generate the name of the verification report for artifact in production
+        vrn=VerificationReport_production_${BUILD_ID}_${ENVIRONMENT_NAME}
+    elif [ "$READY_FOR" == "use" ]; then
+        # Context: Generated for validation environment
+        # Generate the name of the verification report for artifact in validation
+        vrn=VerificationReport_validation_${BUILD_ID}_${ENVIRONMENT_NAME}
+    else
+        # Nothing could be determined, so exit with error
+        vrn=""
+        echo 'ERROR: Could not determine execution context'
+        exit 1
+    fi
+}
+
+
+
+#############
+### START ###
+#############
+
+# Get the parameter values
+ENVIRONMENT_NAME=$1
+BUILD_ID=$2
+READY_FOR=$3
+
+# Guard block: 
+# Check if the parameter values are not empty
+if [[ -z $BUILD_ID || -z $READY_FOR || -z $ENVIRONMENT_NAME ]]; then
+    echo 'One or more parameters are undefined'
+    exit 1
+fi
+
+# Preparation block:
+# Replace all blanks
+ENVIRONMENT_NAME=${ENVIRONMENT_NAME// /_}
+# Replace all dashes
+ENVIRONMENT_NAME=${ENVIRONMENT_NAME//-/_}
+# To lowercase
+ENVIRONMENT_NAME=$(echo "$ENVIRONMENT_NAME" | tr '[:upper:]' '[:lower:]')
+
+# Generate
+generate_verification_report_name "$ENVIRONMENT_NAME" "$BUILD_ID" "$READY_FOR"
+echo $vrn
